8b1143f7ddf7d8b0020f41173a9c1391a0441c78
struggyyy <115942178+struggyyy@users.noreply.github.com>
2023-10-24 21:31:52 +0200

    Music albums were added

---
+++ Lab3/Controllers/AlbumController.cs
@@ -0,0 +1,83 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Lab3.Controllers
+{
+    public class AlbumController : Controller
+    {
+
+        static readonly Dictionary<int, Album> _albums = new Dictionary<int, Album>();
+        static int id = 1;
+
+        public IActionResult Index()
+        {
+            return View(_albums);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Album model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = id++;
+                _albums[model.Id] = model;
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_albums[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Album model)
+        {
+            if (ModelState.IsValid)
+            {
+                _albums[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(_albums[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Album model)
+        {
+            _albums.Remove(model.Id);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(_albums[id]);
+
+        }
+
+        [HttpPost]
+        public void Details(Album model)
+        {
+
+        }
+    }
+
+}
---
+++ Lab3/Models/Album.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Lab3.Models
+{
+    public class Album
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Musisz podać nazwę! ")]
+        [StringLength(maximumLength: 100, ErrorMessage = "Zbyt długa nazwa, podaj mniej niż 100 znaków! ")]
+        public string Name { get; set; }
+
+        [StringLength(maximumLength: 50, ErrorMessage = "Serio taka długa nazwa zespołu? Podaj mniej niż 50 znaków! ")]
+        public string Band { get; set; }
+
+        [AllowNull]
+        public string TrackList { get; set; }
+
+        [Range(0, 100)]
+        public uint Record { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "ReleaseDate")]
+        public DateTime ReleaseDate { get; set; }
+
+        [DataType(DataType.Time)]
+        [Display(Name = "Duration")]
+        public DateTime Duration { get; set; }
+    }
+}
